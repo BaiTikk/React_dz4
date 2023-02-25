75bbdba815af7bc47b0746079b7143092832928d
BaiTikk <bekbolotov146@icloud.com>
2023-02-25 12:04:04 +0600

    dz4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Essayform extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', title: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlepass = this.handlepass.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handlepass(event) {
+    this.setState({title: event.target.value});
+  }
+  handleSubmit(event){
+    alert('Name: '+ this.state.value  + '  password: '+ this.state.title)
+
+    event.preventDefault();
+    this.props.onSubmit(this.state.value);
+    this.setState({value: "", title: ""});
+  }
+  handleClose() {
+    this.setState({open: false});
+  }
+  handleOpen(){
+    this.setState({open: true});
+  }
+  render() {
+    return <>
+      <form className='header' onSubmit={this.handleSubmit}>
+        <div className='inner_header'>
+          <div className='labels'>
+            <label for='inp' className='label' > Name: </label>
+            <input value={this.state.value} onChange={this.handleChange} id='inp' className='inp1'/>
+          </div>
+          <div className='labels'>
+            <label for='inp2'> Password: </label>
+            <input type="text" value={this.state.title} onChange={this.handlepass} id='inp2' />
+          </div>
+        <input type="submit" value="Submit" />
+        </div>
+      </form>
+      </>
+  }
 }
 
-export default App;
+export default Essayform;
